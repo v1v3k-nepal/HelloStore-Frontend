116a3dcb74cc11f1105b6fa1c97f9cecf3c8c1a5
v1v3k <vivekn936@gmail.com>
2023-07-21 19:55:36 +0545

    API Img URL updated with Production URL at search section

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -53,7 +53,7 @@ const Search = ({ setShowSearch }) => {
               <div className="img-container">
                 <img
                   src={
-                    process.env.REACT_APP_DEV_URL +
+                    // process.env.REACT_APP_DEV_URL +
                     item.attributes.img.data[0].attributes.url
                   }
                   alt=""
