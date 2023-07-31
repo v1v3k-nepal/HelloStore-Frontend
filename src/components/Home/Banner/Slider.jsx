71b4483d7c65b1afb46447dc19fdb027b78534e9
v1v3k <vivekn936@gmail.com>
2023-07-31 19:00:18 +0545

    Bug-fix: Horizontal Overflow in Chromium Browsers

--- src/components/Home/Banner/Slider.jsx
+++ src/components/Home/Banner/Slider.jsx
@@ -4,13 +4,12 @@ import Slide2 from "../../../assets/slide2.webp";
 import Slide3 from "../../../assets/slide3.webp";
 import Slide4 from "../../../assets/slide4.webp";
 import Slide5 from "../../../assets/slide5.webp";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const slideImg = [Slide1, Slide2, Slide3, Slide4, Slide5];
-
 
   const navigateSlider = (index) => {
     setCurrentSlide(index);
@@ -18,18 +17,16 @@ const Slider = () => {
 
   return (
     <div className="slider-container">
-      <div className="slide">
-        {slideImg.map((img) => (
-          <>
-            <img src={img} alt="" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}/>
-            <div className="btn-container">
-              {slideImg.map((img, index) => (
-                <button onClick={() => navigateSlider(index) } className={`btn ${currentSlide==index ? "active": ""} `}></button>
-              ))}
-            </div>
-          </>
-        ))}
-      </div>
+      {slideImg.map((img, index) => (
+        <div key={index} className="slide">
+          <img src={img} alt="" style={{ transform: `translateX(-${currentSlide * 100}vw)` }} />
+          <div className="btn-container">
+            {slideImg.map((img, index) => (
+              <button key={index} onClick={() => navigateSlider(index)} className={`btn ${currentSlide === index ? "active" : ""} `}></button>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
